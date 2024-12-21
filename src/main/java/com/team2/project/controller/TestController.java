61fce5b703bf8835992e45c37d15bc79897f2c98
kjy <fare1293@gmail.com>
2024-12-21 14:26:53 +0900

    프론트 수정 및 카카오 로그인 API / 문자 전송 API

--- src/main/java/com/team2/project/controller/TestController.java
+++ src/main/java/com/team2/project/controller/TestController.java
@@ -1,13 +1,38 @@
 package com.team2.project.controller;
 
+import java.util.HashMap;
+
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.servlet.ModelAndView;
+
+import com.kakao.app.KakaoAPI;
+
+import jakarta.servlet.http.HttpSession;
 
 @RestController
 @RequestMapping()
 public class TestController {
-
+	
+	KakaoAPI kakaoApi = new KakaoAPI();
+	
+	@GetMapping(value="/login")
+	public ModelAndView login(@RequestParam("code") String code, HttpSession session) {
+		ModelAndView mav = new ModelAndView();
+		// 1번 인증코드 요청 전달
+		String accessToken = kakaoApi.getAccessToken(code);
+		// 2번 인증코드로 토큰 전달
+		HashMap<String, Object> userInfo = kakaoApi.getUserInfo(accessToken);
+		
+		System.out.println("login info : " + userInfo.toString());
+		mav.addObject("userId");
+		mav.setViewName("index");
+		return mav;
+	}
+	
+	
 	@RequestMapping(value="/join")
 	public ModelAndView join() {
 		ModelAndView mav = new ModelAndView();
