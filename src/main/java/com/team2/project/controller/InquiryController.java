dc5ab9391aea4a15c471853ee161432ab5f384b1
WOODY-GB4P\WOODY <kingright8@naver.com>
2024-12-31 15:02:07 +0900

    경인 - 레포지토리, 서비스, 컨트롤러 수정

--- src/main/java/com/team2/project/controller/InquiryController.java
+++ src/main/java/com/team2/project/controller/InquiryController.java
@@ -2,9 +2,7 @@ package com.team2.project.controller;
 
 import java.time.LocalDate;
 import java.util.List;
-import java.util.Optional;
 
-import org.springframework.format.annotation.DateTimeFormat;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
@@ -13,10 +11,13 @@ import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.SessionAttribute;
+import org.springframework.web.bind.annotation.SessionAttributes;
 import org.springframework.web.multipart.MultipartFile;
+import org.springframework.web.servlet.mvc.support.RedirectAttributes;
 
 import com.team2.project.model.Inquiry;
 import com.team2.project.model.InquiryCategory;
+import com.team2.project.model.InquiryStatus;
 import com.team2.project.model.Member;
 import com.team2.project.service.InquiryService;
 
@@ -25,6 +26,7 @@ import lombok.RequiredArgsConstructor;
 @RequiredArgsConstructor
 @Controller
 @RequestMapping("/inquiry")
+@SessionAttributes("loginOK")
 public class InquiryController {
 	
 	private final InquiryService inquiryService;
@@ -32,98 +34,136 @@ public class InquiryController {
     // 1. 문의 조회 카테고리 버튼을 클릭했을 때 -> 조회 페이지로 이동 (날짜로 검색 x)
     @GetMapping
     public String findAllList(
-    		@SessionAttribute("재영님로그인확인") Member member,
+    		@SessionAttribute("loginOK") Member member,
+    		@RequestParam(required = false, defaultValue = "ALL") String status,
     		Model model) {
-        List<Inquiry> inquirys = inquiryService.findInquiry(member.getMemberNo());
+    	
+    	 List<Inquiry> inquirys;
+    	 
+    	switch(status) {
+    	case "ALL":
+    		inquirys = inquiryService.findInquiry(member.getMemberNo());
+    		break;
+    	
+    	case "PROCESSING":
+    		inquirys = inquiryService.findInquiryByStatus(member.getMemberNo(), InquiryStatus.ANSWER_PROCESSING);
+    		break;
+    		
+    	case "CHOSEN":
+    		inquirys = inquiryService.findInquiryByStatus(member.getMemberNo(), InquiryStatus.ANSWER_CHOSEN);
+    		break;
+    	
+    	default:
+    		inquirys = inquiryService.findInquiry(member.getMemberNo());
+    		break;
+    	}
+    	
         model.addAttribute("inquirys", inquirys);
-        return "/inquiry/list";
+        return "inquiry/list";
     }
     
     
     // 2. 조회 페이지에서 날짜 범위 선택 후, '조회' 버튼 눌렀을 때
     @GetMapping("/findByDate")
     public String findAllListByDate(
-    		@SessionAttribute("재영님로그인확인") Member member, 
-    		@DateTimeFormat(pattern = "yyyy-MM-dd") @RequestParam LocalDate startDate,
-    		@DateTimeFormat(pattern = "yyyy-MM-dd") @RequestParam LocalDate endDate,
+    		@SessionAttribute("loginOK") Member member, 
+    		@RequestParam LocalDate startDate,
+    		@RequestParam LocalDate endDate,
     		Model model) {
     	
         List<Inquiry> inquirys = inquiryService.findInquiryByDate(member.getMemberNo(), startDate, endDate);
         model.addAttribute("inquirys", inquirys);
-        return "/inquiry/list";
+        return "redirect:/inquiry";
     }
     
     
     // 3. 문의 접수 카테고리 버튼을 클릭했을 때 -> 접수 페이지로 이동
     @GetMapping("/add")
     public String moveToAddForm(
-    		@SessionAttribute("재영님로그인확인") Member member) {
-        return "/inquiry/add";
+    		@SessionAttribute("loginOK") Member member) {
+        return "inquiry/add";
     }
     
     
     // 4. 문의 접수 페이지에서, 접수하기 버튼을 클릭했을 때 -> 접수 완료 -> 조회 페이지로 리디렉션
     @PostMapping("/add")
     public String addInquiry(
-    		@SessionAttribute("재영님로그인확인") Member member,
+    		@SessionAttribute("loginOK") Member member,
     		@RequestParam InquiryCategory inquiryCategory,
 			@RequestParam String inquiryTitle,
 			@RequestParam String inquiryContent,
-			@RequestParam List<MultipartFile> files) {
-    	inquiryService.saveInquiry(member, inquiryCategory, inquiryTitle, inquiryContent, files);
+			@RequestParam List<MultipartFile> files,
+			@RequestParam(required = false) Integer inquiryNo) {
+    	inquiryService.saveInquiry(member, inquiryCategory, inquiryTitle, inquiryContent, files, inquiryNo);
     	
-        return "redirect:/inquiry/list";
+        return "redirect:/inquiry";
+
     }
     
     
     // 5. 문의 조회 페이지에서, 수정 버튼을 눌렀을 때 -> 수정 페이지로 이동
     @GetMapping("/edit/{inquiryNo}")
     public String moveToEditForm(
-    		@SessionAttribute("재영님로그인확인") Member member,
+    		@SessionAttribute("loginOK") Member member,
     		@PathVariable int inquiryNo,
     		Model model) {
     	
-    	Optional<Inquiry> inquiry = inquiryService.findById(inquiryNo);
-    	if(inquiry.isEmpty()) {
-    		// 혹시나 수정하기 전 원본 내용이 비어있을 경우
-    		return null;
-    	}
-    	
-    	model.addAttribute("inquiry", inquiry);
-    	
-        return "/inquiry/edit";
+    	try {
+            Inquiry inquiry = inquiryService.findById(inquiryNo);
+            
+            // 현재 로그인한 사용자가 해당 문의의 작성자인지 확인
+            if (inquiry.getMember().getMemberNo() != member.getMemberNo()) {
+                throw new IllegalArgumentException("회원이 일치하지 않습니다.");
+            }
+            
+            model.addAttribute("inquiry", inquiry);
+            return "inquiry/edit";
+        } catch (IllegalArgumentException e) {
+            model.addAttribute("errorMessage", e.getMessage());
+            return "inquiry/error";
+        }
     }
     
     
     // 6. 문의 수정 페이지에서, 수정 후 접수하기 버튼을 클릭했을 때 -> 수정 완료 -> 조회 페이지로 리디렉션
     @PostMapping("/edit")
     public String editInquiry(
-    		@SessionAttribute("재영님로그인확인") Member member,
-    		@RequestParam InquiryCategory inquiryCategory,
-			@RequestParam String inquiryTitle,
-			@RequestParam String inquiryContent,
-			@RequestParam List<MultipartFile> files) {
-    	
-    	inquiryService.saveInquiry(member, inquiryCategory, inquiryTitle, inquiryContent, files);
-    	
-        return "redirect:/inquiry/list";
+            @SessionAttribute("loginOK") Member member,
+            @RequestParam InquiryCategory inquiryCategory,
+            @RequestParam String inquiryTitle,
+            @RequestParam String inquiryContent,
+            @RequestParam List<MultipartFile> files,
+            @RequestParam Integer inquiryNo,
+            RedirectAttributes redirectAttributes,
+            Model model) {
+
+        try {
+            inquiryService.saveInquiry(member, inquiryCategory, inquiryTitle, inquiryContent, files, inquiryNo);
+        } catch (IllegalArgumentException e) {
+            model.addAttribute("errorMessage", e.getMessage());
+            redirectAttributes.addAttribute("inquiryNo", inquiryNo);
+            return "redirect:/inquiry/edit/{inquiryNo}"; // 에러 메시지를 가지고 수정 페이지로 돌아감
+        }
+
+        return "redirect:/inquiry";
     }
+
 			
 
 	// 7. 문의 조회 페이지에서, 삭제 버튼을 눌렀을 때 -> 조회 페이지로 리디렉션
 	@PostMapping("/delete/{inquiryNo}")
 	public String deleteInquiry(
 			@PathVariable int inquiryNo,
-			@SessionAttribute("재영님로그인확인") Member member) {
+			@SessionAttribute("loginOK") Member member) {
 		
 		inquiryService.deleteInquiry(member.getMemberNo(), inquiryNo);
 		
-		return "redirect:/inquiry/list";
+		return "redirect:/inquiry";
 	}
 	
 
 	// 8. 관리자 페이지에서 해당 문의에 답변 후 '등록'버튼 클릭했을 때
-	@PostMapping("/admin/")
+	@PostMapping("/admin")
 	public String addInquiryAnswerByAdmin(
 			@PathVariable int inquiryNo,
 			@RequestParam String inquiryAnswer,
