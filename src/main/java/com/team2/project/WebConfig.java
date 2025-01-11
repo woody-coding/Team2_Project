05600ce655bbb59a1e08a15fc0aa4ac664678492
WOODY-GB4P\WOODY <kingright8@naver.com>
2025-01-11 16:30:43 +0900

    경인 - 로그인 인터셉터 관련 수정, 헤더/푸터 추가

--- src/main/java/com/team2/project/WebConfig.java
+++ src/main/java/com/team2/project/WebConfig.java
@@ -2,6 +2,7 @@ package com.team2.project;
 
 import org.springframework.context.annotation.Configuration;
 import org.springframework.web.servlet.config.annotation.InterceptorRegistry;
+import org.springframework.web.servlet.config.annotation.ResourceHandlerRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
 import com.team2.project.intercepter.LoginCheckInterceptor;
@@ -9,11 +10,19 @@ import com.team2.project.intercepter.LoginCheckInterceptor;
 @Configuration
 public class WebConfig implements WebMvcConfigurer {
 
-    @Override
-    public void addInterceptors(InterceptorRegistry registry) {
-        registry.addInterceptor(new LoginCheckInterceptor())
-                .addPathPatterns("/**") // 모든 경로에 대해 인터셉터 적용
-                .excludePathPatterns("/", "/login", "/join", "/css/**", "/js/**", "/images/**", "/imgs/**", "/kakaologin", "/findID",
-                						"/findPW", "/phonechk", "/insert", "/searchId", "/searchIdPW", "/idchk"); // 제외할 경로 지정
-    }
+	@Override
+	public void addInterceptors(InterceptorRegistry registry) {
+	    registry.addInterceptor(new LoginCheckInterceptor())
+	            .addPathPatterns("/**")
+	            .excludePathPatterns("/", "/login", "/join", "/css/**", "/js/**", "/images/**", "/imgs/**", "/kakaologin", "/findID",
+	                                "/findPW", "/phonechk", "/insert", "/searchId", "/searchIdPW", "/idchk", "/ticketMenu", "/ticketMenu/{showCate}",
+	                                "/ticketDetailPage/{showNo}", "/show/Main", "/actor/{actorNo}");
+	}
+
+    // * 이미지 파일 업로드 경로를	>>	C:/finalFile/ 폴더로 경로로 설정할 때
+//    @Override
+//    public void addResourceHandlers(ResourceHandlerRegistry registry) {
+//        registry.addResourceHandler("/finalFile/**")
+//                .addResourceLocations("file:C:/finalFile/");
+//    }
 }
