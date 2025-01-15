aeaab056581b167b8ea3961c456af898e33a44f8
kjy <fare1293@gmail.com>
2025-01-15 17:40:47 +0900

    no message

--- src/main/java/com/team2/project/controller/MemberController.java
+++ src/main/java/com/team2/project/controller/MemberController.java
@@ -4,7 +4,6 @@ import java.util.Date;
 import java.util.HashMap;
 import java.util.Optional;
 
-import org.hibernate.tool.schema.internal.AbstractSchemaValidator;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
