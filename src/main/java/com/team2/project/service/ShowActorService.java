0016d58c55c208f929e31addbeed8e9d30d338dc
docong1002 <97docong@gmail.com>
2025-01-16 14:48:06 +0900

    공연 등록시 배우 사진 안나오는거 수정

--- src/main/java/com/team2/project/service/ShowActorService.java
+++ src/main/java/com/team2/project/service/ShowActorService.java
@@ -1,6 +1,8 @@
 package com.team2.project.service;
 
+import com.team2.project.model.Actor;
 import com.team2.project.model.ShowActor;
+import com.team2.project.model.ShowActorFile;
 import com.team2.project.repository.ShowActorRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
@@ -15,8 +17,23 @@ public class ShowActorService {
     private ShowActorRepository showActorRepository;
 
     public List<ShowActor> getActorsByShowNo(int showNo) {
-        return showActorRepository.findByShowNo(showNo);
+        // showNo로 ShowActor 목록을 조회
+        List<ShowActor> showActors = showActorRepository.findByShowNo(showNo);
+
+        // ShowActor 목록을 순회하며 각 ShowActorFile의 filePath를 처리
+        for (ShowActor showActor : showActors) {
+            // showActor에서 ShowActorFile 가져오기
+            if (showActor.getActor() != null && showActor.getActor().getShowActorFile() != null) {
+                ShowActorFile showActorFile = showActor.getActor().getShowActorFile();
+                String filePath = showActorFile.getFilePath();
+                // filePath를 ShowActor에 설정하거나 반환에 포함
+                // 예: showActor.setFilePath(filePath); // 필요시 추가
+            }
+        }
+
+        return showActors;
     }
+
 
     @Transactional
     public void saveShowActor(ShowActor showActor) {
